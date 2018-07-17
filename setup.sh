e9cceda9b2f120eebf2b63138ff9b091142aacac
asingh1007 <32540749+asingh1007@users.noreply.github.com>
2018-07-17 18:46:43 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo yum install tomcat7 tomcat7-webapps -y
-sudo rm -rf /usr/share/tomcat7/webapps/CustomerOnBoard-Raghib-0.0.1*
+sudo yum install tomcat8 tomcat8-webapps -y
+sudo rm -rf /usr/share/tomcat8/webapps/CustomerOnBoard-Raghib-0.0.1*
